5ad27a8156031cd97e32327b92f4afda7df4f338
Pratyush Garg <pratyushhg@gmail.com>
2021-05-31 23:26:20 +0530

    add react refresh for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const MiniExtractCssPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 let mode = "development";
 let target = "web";
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniExtractCssPlugin(),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+];
 if (process.env.NODE_ENV === "production") {
   mode = "production";
   target = "browserslist";
+} else {
+  plugins.push(new ReactRefreshWebpackPlugin());
 }
 
 module.exports = {
   mode: mode,
   target: target,
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]",
@@ -54,13 +65,7 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniExtractCssPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  plugins: plugins,
   devtool: "source-map",
   devServer: {
     hot: true,
